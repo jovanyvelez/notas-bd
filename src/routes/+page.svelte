41c3447b41e6a92c51afb6ffd412dd538de62b55
Jovany <jovany.velez@gmail.com>
2024-07-16 11:18:04 -0500

    test3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,16 @@
+<h1 class="titulo">Notas del Curso de Bases de Datos</h1>
 
 
+<a class="textsize" href="/sesiones/1">Clase 1</a>
 
-<h1 class="my-element">Notas del Curso de Bases de Datos</h1>
+<style>
+	.titulo {
+		text-align: center;
+		margin-left: auto;
+		margin-right: auto;
+	}
 
-
-
-  <a class="inicio" href="/sesiones/1">Clase 1</a>
-
- 
-
-  <style lang="postcss">
-    .my-element {
-      @apply text-center;
-      @apply text-3xl;
-	  @apply font-bold;
-	  @apply bg-blue-200;
-    }
-
-    .inicio {
-      @apply link;
-      @apply text-center;
-      @apply text-3xl;
-	  @apply font-bold;
-	  @apply bg-blue-200;
-    }
+	.textsize {
+		font-size: 20px;
+	}
 </style>
